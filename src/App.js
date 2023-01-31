c43902af554ee7d0bdec5bb9b83b408a862b1f8b
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-01-31 19:58:04 +0530

    second  commit

--- src/App.js
+++ src/App.js
@@ -2,45 +2,94 @@ import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 
 const steps = [
-	{
-		id: '0',
-		message: 'Hey Geek!',
 
-		// This calls the next id
-		// i.e. id 1 in this case
-		trigger: '1',
-	}, {
-		id: '1',
+  {
 
-		// This message appears in
-		// the bot chat bubble
-		message: 'Please write your username',
-		trigger: '2'
-	}, {
-		id: '2',
+    id: "Greet",
 
-		// Here we want the user
-		// to enter input
-		user: true,
-		trigger: '3',
-	}, {
-		id: '3',
-		message: " hi {previousValue}, how can I help you?",
-		trigger: 4
-	}, {
-		id: '4',
-		options: [
-			
-			// When we need to show a number of
-			// options to choose we create alist
-			// like this
-			{ value: 1, label: 'View Courses' },
-			{ value: 2, label: 'Read Articles' },
+    message: "Hello, Welcome to our shop",
 
-		],
-		end: true
-	}
-];
+    trigger: "Done",
+
+  },
+
+  {
+
+    id: "Done",
+
+    message: "Please enter your name!",
+
+    trigger: "waiting1",
+
+  },
+
+  {
+
+    id: "waiting1",
+
+    user: true,
+
+    trigger: "Name",
+
+  },
+
+  {
+
+    id: "Name",
+
+    message: "Hi {previousValue}, Please select your issue",
+
+    trigger: "issues",
+
+  },
+
+  {
+
+    id: "issues",
+
+    options: [
+
+      {
+
+        value: "React",
+
+        label: "React",
+
+        trigger: "React",
+
+      },
+
+      { value: "Angular", label: "Angular", trigger: "Angular" },
+
+    ],
+
+  },
+
+  {
+
+    id: "React",
+
+    message:
+
+      "Thanks for letting your React issue, Our team will resolve your issue ASAP",
+
+    end: true,
+
+  },
+
+  {
+
+    id: "Angular",
+
+    message:
+
+      "Thanks for letting your Angular issue, Our team will resolve your issue ASAP",
+
+    end: true,
+
+  },
+
+]; 
 
 // Creating our own theme
 const theme = {
